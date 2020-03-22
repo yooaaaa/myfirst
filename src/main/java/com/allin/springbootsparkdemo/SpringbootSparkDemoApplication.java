ccc89dcc71b1c7618f82813fb216570643dc9d18
allin <yooaaaa@163.com>
2020-03-22 18:05:44 +0800

    我的第一个项目提交

---
+++ src/main/java/com/allin/springbootsparkdemo/SpringbootSparkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.allin.springbootsparkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSparkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSparkDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/spark/WordCount.java
@@ -0,0 +1,14 @@
+package spark;
+
+/**
+ * @author yangxuelian
+ * @Date ${date} ${time}
+ */
+public class WordCount
+
+{
+
+    def  main(args:Array[String]:  Unit={
+
+    })
+}
